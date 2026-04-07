65b597d89e24cd681a96c8b445560d90f49cbe26
wlasah <agan.lawrence147@gmail.com>
2026-04-08 00:28:28 +0800

    Add pip install to build.sh for dependency installation on Render

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Render build script - runs before starting the web service
 
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
 echo "Starting Django migrations..."
 python manage.py migrate
 
